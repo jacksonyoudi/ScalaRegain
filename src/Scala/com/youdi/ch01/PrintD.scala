f24ba0d2417136685d3e087f3e8a58a9157a23bf
jacksonyoudi <liangchangyoujackson@gmail.com>
2020-11-24 23:36:49 +0800

    init

---
+++ src/Scala/com/youdi/ch01/PrintD.scala
@@ -0,0 +1,16 @@
+package com.youdi.ch01
+
+object PrintD {
+  def main(args: Array[String]): Unit = {
+    val str1: String = "hello"
+    val str2: String = " world"
+    println(str1 + str2)
+
+    val name: String = "youdi"
+    val age: Int = 28
+
+    printf("name:%s,age is %d", name, age)
+
+    println(s"name is $name, age is ${age}")
+  }
+}
