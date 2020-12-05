3074d4d0926bc64a4f77a78fd788246e1e39821c
jacksonyoudi <liangchangyoujackson@gmail.com>
2020-12-05 10:59:14 +0800

    operations

---
+++ src/Scala/com/youdi/ch03/arithoperation/Demo01.scala
@@ -0,0 +1,16 @@
+package com.youdi.ch03.arithoperation
+
+object Demo01 {
+  def main(args: Array[String]): Unit = {
+    val i: Int = 10 / 3 // 3
+    val d: Double = 10 / 3 // 3.0
+    val r: Double = 10.0 / 3 // 3.333
+    println(r.formatted("%.2f"))
+
+    // % 使用  a % b = a - a/b * b
+
+
+    //scala中+=1 -=1
+
+  }
+}
