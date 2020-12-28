07f1a4fca7a35ffb42928b407af7a499d766772f
jacksonyoudi <liangchangyoujackson@gmail.com>
2020-12-28 21:18:14 +0800

    for

---
+++ src/main/Scala/com/youdi/ch04/forf/D07.scala
@@ -0,0 +1,16 @@
+package com.youdi.ch04.forf
+
+object D07 {
+  def main(args: Array[String]): Unit = {
+    for (i <- 1 to 3; j = i * 2) {
+      println("i:" + i + " j:" + j)
+    }
+
+    // 等价
+    for {i <- 1 to 3
+         j = i * 2} {
+      println("i:" + i + " j:" + j)
+    }
+  }
+
+}
