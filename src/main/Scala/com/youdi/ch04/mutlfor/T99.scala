611a6f5da0ca960a4345b2163755742fc054209b
jacksonyoudi <liangchangyoujackson@gmail.com>
2020-12-29 08:28:04 +0800

    99

--- src/main/Scala/com/youdi/ch04/mutlfor/T99.scala
+++ src/main/Scala/com/youdi/ch04/mutlfor/T99.scala
@@ -3,8 +3,14 @@ package com.youdi.ch04.mutlfor
 object T99 {
   def main(args: Array[String]): Unit = {
     for (i <- 1 until 10) {
-      for (j <- 1 until 10) {
-        printf(i + " * " + j + " = " + (i * j) + " ")
+//      for (j <- 1 until 10) {
+//        printf(i + " * " + j + " = " + (i * j) + " ")
+//      }
+//      println()
+
+
+      for (j <- 1 to i ) {
+        printf(j + " * " + i + " = " + (i * j) + " ")
       }
       println()
     }
