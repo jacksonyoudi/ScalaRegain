eb42f1f42fdc31c7a73a2240f56b1fd8729b83a7
jacksonyoudi <liangchangyoujackson@gmail.com>
2020-12-30 08:25:59 +0800

    final private

---
+++ src/main/Scala/com/youdi/ch05/detail/D01.scala
@@ -0,0 +1,19 @@
+package com.youdi.ch05.detail
+
+object D01 {
+  def main(args: Array[String]): Unit = {
+
+    def f1(): Unit = {
+      println("f1")
+    }
+
+    println("ok")
+
+  }
+
+  def sayHello(): Unit = {
+    println("Hello")
+  }
+
+
+}
