7932ab12fce4c1d100af4a5b2dee7e54a1ff363b
jacksonyoudi <liangchangyoujackson@gmail.com>
2020-12-30 09:03:37 +0800

    lazy

--- src/main/Scala/com/youdi/ch05/mlazy/D01.scala
+++ src/main/Scala/com/youdi/ch05/mlazy/D01.scala
@@ -1,5 +1,10 @@
 package com.youdi.ch05.mlazy
 
+
+/**
+ * lazy不能修饰var类型的变量
+ * 不但在调用函数时，加了lazy，会导致函数的执行被推迟，我们在声明一个变量时，如果给声明了lazy，那么变量值也会被分配延迟。
+ */
 object D01 {
   def main(args: Array[String]): Unit = {
     lazy val res = sum(1, 2)
