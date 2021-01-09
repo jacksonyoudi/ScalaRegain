3650fe1db7b41a94c149f93aa74494377e32f76a
jacksonyoudi <liangchangyoujackson@gmail.com>
2021-01-09 08:50:09 +0800

    oop

--- src/main/Scala/com/youdi/ch06/oop/CatDemo.scala
+++ src/main/Scala/com/youdi/ch06/oop/CatDemo.scala
@@ -1,5 +1,12 @@
 package com.youdi.ch06.oop
 
+
+/**
+ * 类是抽象的，概念的，代表一类事物
+ * 对象是具体的，实际的，代表一个具体的事物
+ * 类是对象的模板，对象是类的一个个个体，对应一个实例
+ * scala中中类和对象的区别和联系和java中一样的
+ */
 object CatDemo {
   def main(args: Array[String]): Unit = {
     val cat = new Cat
@@ -14,7 +21,7 @@ object CatDemo {
  *
  * 1. 当我们声明 var name: String时，在底层对应 private name
  * 2. 同时会生成 private name 并且会生成 public name() get和set的方法
- *
+ * 3. public name_$eq("")
  *
  */
 
