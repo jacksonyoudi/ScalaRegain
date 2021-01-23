43ba93eff161b0d2236dbc6226ca45f1088d696c
jacksonyoudi <liangchangyoujackson@gmail.com>
2021-01-23 23:26:43 +0800

    override

--- src/main/java/youdi/com/App.java
+++ src/main/java/youdi/com/App.java
@@ -2,12 +2,9 @@ package youdi.com;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
