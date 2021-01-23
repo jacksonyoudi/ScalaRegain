1831136cc06fef92599816af22c2a24bd4d5e0a9
jacksonyoudi <liangchangyoujackson@gmail.com>
2021-01-23 23:45:12 +0800

    trait

---
+++ src/main/Scala/com/youdi/ch06/interfaced/D01.java
@@ -0,0 +1,10 @@
+package com.youdi.ch06.interfaced;
+
+/**
+ * interface
+ *
+ * implements
+ */
+public class D01 {
+
+}
---
+++ src/main/Scala/com/youdi/ch06/interfaced/D02.scala
@@ -0,0 +1,11 @@
+package com.youdi.ch06.interfaced
+
+
+/**
+ * scala中没有接口
+ * 1. scala采用特质trait  替代接口概念 (interface  + abstract class)
+ * 2. 实现 + 动态混入(mixin)
+ */
+object D02 {
+
+}
