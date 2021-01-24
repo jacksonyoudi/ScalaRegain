d77292882eddfba23ecc045413787eadf41dd428
jacksonyoudi <liangchangyoujackson@gmail.com>
2021-01-24 23:42:08 +0800

    implicit value

---
+++ src/main/Scala/com/youdi/ch10/collectpkg/D01.scala
@@ -0,0 +1,14 @@
+package com.youdi.ch10.collectpkg
+
+/**
+ * 1. scala支持可变集合和不可变集合，不可变集合可以安全的并发访问
+ * 2. 两个主要的包
+ *    scala.collection.mutable
+ *    scala.collection.immutable
+ * 3. scala默认采用不可变集合，对于几乎所有的集合类，scala都同时提供了可变mutable和不可变immutable的版本
+ * 4. scala的集合有三大类，序seq, 集合set，映射map, 所有的集合都扩展自iterable特质。 在scala中集合有可变mutable和imutable不可变两种类型。
+ */
+object D01 {
+
+
+}
