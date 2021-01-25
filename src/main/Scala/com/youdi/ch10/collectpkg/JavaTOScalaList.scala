b2d319b6228f2536ee7881db714ad213f933cdde
jacksonyoudi <liangchangyoujackson@gmail.com>
2021-01-25 20:56:49 +0800

    array to java

--- src/main/Scala/com/youdi/ch10/collectpkg/JavaTOScalaList.scala
+++ src/main/Scala/com/youdi/ch10/collectpkg/JavaTOScalaList.scala
@@ -4,6 +4,10 @@ package com.youdi.ch10.collectpkg
 import java.util
 import scala.collection.mutable.ArrayBuffer
 
+
+/**
+ * 类继承了trait， 实例的对象， 可以传给trait
+ */
 object JavaTOScalaList {
   def main(args: Array[String]): Unit = {
 //    val bf: ArrayBuffer[String] = ArrayBuffer("1", "2", "3")
