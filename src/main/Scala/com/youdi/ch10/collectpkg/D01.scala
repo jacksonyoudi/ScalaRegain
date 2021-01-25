a33306a0eaf10d533db1ae5dac427dc9b57cb2c4
jacksonyoudi <liangchangyoujackson@gmail.com>
2021-01-25 08:42:00 +0800

    collection

--- src/main/Scala/com/youdi/ch10/collectpkg/D01.scala
+++ src/main/Scala/com/youdi/ch10/collectpkg/D01.scala
@@ -7,8 +7,14 @@ package com.youdi.ch10.collectpkg
  *    scala.collection.immutable
  * 3. scala默认采用不可变集合，对于几乎所有的集合类，scala都同时提供了可变mutable和不可变immutable的版本
  * 4. scala的集合有三大类，序seq, 集合set，映射map, 所有的集合都扩展自iterable特质。 在scala中集合有可变mutable和imutable不可变两种类型。
+ *
+ *
+ *
+ * 不可变集合：集合本身不能动态变化, 内容可变。 hashcode变化。
+ * 可变集合： 集合可以动态变化 hashcode改变
  */
 object D01 {
+  def main(args: Array[String]): Unit = {
 
-
+  }
 }
---
+++ src/main/Scala/com/youdi/ch10/collectpkg/JavaD01.java
@@ -0,0 +1,17 @@
+package com.youdi.ch10.collectpkg;
+
+import java.util.ArrayList;
+
+public class JavaD01 {
+    public static void main(String[] args) {
+        int[] nums = new int[3];
+
+
+        ArrayList<String> list = new ArrayList<String>();
+        list.add("test");
+        list.add("test");
+        list.add("test");
+        list.add("test");
+
+    }
+}
