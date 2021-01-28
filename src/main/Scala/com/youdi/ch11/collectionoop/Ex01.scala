1ea96c410746db10bfb1ea088506a3051d83f3a6
jacksonyoudi <liangchangyoujackson@gmail.com>
2021-01-28 22:59:58 +0800

    fold

--- src/main/Scala/com/youdi/ch11/collectionoop/Ex01.scala
+++ src/main/Scala/com/youdi/ch11/collectionoop/Ex01.scala
@@ -1,9 +1,46 @@
 package com.youdi.ch11.collectionoop
 
+import scala.collection.mutable
+import scala.collection.mutable.{ArrayBuffer, Map}
+
 object Ex01 {
   def main(args: Array[String]): Unit = {
     val l1: List[String] = List("alice", "bob", "NIck")
     val l2: List[String] = l1.map(_.toUpperCase)
     println(l2)
+
+
+    val a: String = "AAAAAAAAAAABBBBBBCCCCDDDDDD"
+
+    val b: ArrayBuffer[Char] = new ArrayBuffer[Char]()
+    a.foldLeft(b)((b, a) => {
+      b.addOne(a)
+    })
+
+    println(b)
+
+
+    val c: String = "abcdbdddbadddddcccdddvvvadddavdssssnnn"
+
+    val map: mutable.Map[Char, Int] = mutable.Map[Char, Int]()
+
+    //    c.foldLeft(map)(
+    //      (map, b) => {
+    //        if (map.contains(b)) {
+    //          map(b) += 1
+    //        } else {
+    //          map(b) = 1
+    //        }
+    //        map
+    //      }
+    //    )
+
+    c.foldLeft(map)((map, b) => {
+      map(b) = (map.getOrElse(b, 0) + 1)
+      map
+    })
+
+
+    println(map)
   }
 }
