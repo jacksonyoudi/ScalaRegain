42534381436dd73631b4e9d423ee3cce7be5b3fa
jacksonyoudi <liangchangyoujackson@gmail.com>
2021-01-28 22:09:28 +0800

    reduce 01

--- src/main/Scala/com/youdi/ch11/collectionoop/ReduceD01.scala
+++ src/main/Scala/com/youdi/ch11/collectionoop/ReduceD01.scala
@@ -7,14 +7,28 @@ package com.youdi.ch11.collectionoop
  *
  * reduceLeft:   def reduceLeft[B >: A](op: (B, A) => B): B = {
  *
+ *   def reduceRight[B >: A](op: (A, B) => B): B = {
+ *
  *
  */
 
 object ReduceD01 {
   def main(args: Array[String]): Unit = {
     val list: List[Int] = (1 to 10).toList
-//    val i: Int = list.reduceLeft((a, b) => a + b)
-    val i: Int = list.reduceLeft(_ + _)
+    //    val i: Int = list.reduceLeft((a, b) => a + b)
+//    val i: Int = list.reduceLeft(_ + _)
+    val i: Int = list.reduceLeft(add)
+//    val i: Int = list.reduceLeft(+)
     println(i)
+
+    val i1: Int = list.reduceLeft(math.min(_, _))
+    println(i1)
+
+
+  }
+
+
+  def add(a: Int, b: Int): Int = {
+    a + b
   }
 }
