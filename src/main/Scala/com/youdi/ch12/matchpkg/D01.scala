ea195721db632e0c3a4439551142bc4c3ee1774f
jacksonyoudi <liangchangyoujackson@gmail.com>
2021-01-30 21:26:04 +0800

    match

--- src/main/Scala/com/youdi/ch12/matchpkg/D01.scala
+++ src/main/Scala/com/youdi/ch12/matchpkg/D01.scala
@@ -1,5 +1,13 @@
 package com.youdi.ch12.matchpkg
 
+
+/**
+ * 1. 如果不配，那么会执行case _
+ * 2. 如果所有case都不匹配，又没有写case _ 分支，那么会抛出 MatchError
+ * 3. 每个case中，不用break语句，自动中断case
+ * 4. 可以在match中使用其他类型，而不仅仅是字符
+ * 5. =>
+ */
 object D01 {
   def main(args: Array[String]): Unit = {
     val n1: Int = 10
@@ -19,6 +27,7 @@ object D01 {
     val i: Int = op match {
       case "+" => n1 + n2
       case "*" => n1 * n2
+      case 10 => 10
       case _ => 0
     }
 
