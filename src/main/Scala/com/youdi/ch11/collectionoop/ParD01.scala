8e2915866487e22903129698849ce6fc54cdf03d
jacksonyoudi <liangchangyoujackson@gmail.com>
2021-01-30 17:34:52 +0800

    par

--- src/main/Scala/com/youdi/ch11/collectionoop/ParD01.scala
+++ src/main/Scala/com/youdi/ch11/collectionoop/ParD01.scala
@@ -7,13 +7,13 @@ object ParD01 {
   def main(args: Array[String]): Unit = {
     val r1: IndexedSeq[String] = (1 to 100).map {
       case _ => Thread.currentThread.getName
-    }
+    }.distinct
 
     println(r1)
 
     val r2: ParSeq[String] = (1 to 100).par.map {
       case _ => Thread.currentThread.getName
-    }
+    }.distinct
 
     println(r2)
 
