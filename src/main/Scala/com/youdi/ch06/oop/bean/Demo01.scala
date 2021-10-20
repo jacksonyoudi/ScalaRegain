9046ff33762067e4405db994c69d4693cb730740
jacksonyoudi <liangchangyoujackson@gmail.com>
2021-10-20 09:21:56 +0800

    jvm

--- src/main/Scala/com/youdi/ch06/oop/bean/Demo01.scala
+++ src/main/Scala/com/youdi/ch06/oop/bean/Demo01.scala
@@ -2,6 +2,14 @@ package com.youdi.ch06.oop.bean
 
 import scala.beans.BeanProperty
 
+
+/**
+ * 对象创建的流程分析
+ * 1. 加载类的信息(属性信息，方法和属性)
+ * 2. 在内存堆开辟空间 （方法区）
+ * 3. 构造器初始化对象属性
+ * 4.
+ */
 object Demo01 {
   def main(args: Array[String]): Unit = {
     val car: Car = new Car()
